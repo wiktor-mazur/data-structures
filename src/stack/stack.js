4657a34b449eae0d90ff8fd3faadea7bec65bdc1
Viktor <Nivelis91@gmail.com>
2019-03-20 23:51:31 +0100

    Adds Stack

---
+++ src/stack/stack.js
@@ -0,0 +1,100 @@
+/**
+ * Many operations could have been done much easier in JavaScript using methods and properties like
+ * Array.prototype.find, .push, .pop, .length etc. but I wanted to keep it low-level and not use them.
+ */
+
+class Stack {
+  constructor() {
+    this._storage = [];
+    this._top = -1;
+  }
+
+  /**
+   * Returns top element from stack without removing it.
+   *
+   * @returns {null | * }
+   */
+  peek() {
+    return this.isEmpty() ? null : this._storage[this._top];
+  }
+
+  /**
+   * Returns given item's distance from the top of the stack.
+   * The topmost item on the stack is considered to be at distance 1.
+   *
+   * @param { * } item
+   *
+   * @returns { number } Returns -1 when element was not found.
+   */
+  search(item) {
+    for (let i = 0; i <= this._top; ++i) {
+      if (item === this._storage[this._top - i]) {
+        return i + 1;
+      }
+    }
+
+    return -1;
+  }
+
+  /**
+   * Checks whether the stack is empty.
+   *
+   * @returns { boolean }
+   */
+  isEmpty() {
+    return this._top === -1;
+  }
+
+  /**
+   * Returns stack's size.
+   *
+   * @returns { number }
+   */
+  size() {
+    return this._top + 1;
+  }
+
+  /**
+   * Pushes given item to the top of the stack.
+   *
+   * @param { * } item
+   *
+   * @returns Returns the item argument.
+   */
+  push(item) {
+    this._storage[++this._top] = item;
+
+    return item;
+  }
+
+  /**
+   * Removes the topmost element from the stack and returns it.
+   *
+   * @returns { null | *} Returns null when the stack is empty.
+   */
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const lastItem = this._storage[this._top];
+    this._storage.length = this._top--;
+    return lastItem;
+  }
+
+  /**
+   * Swaps places of the top two element on the stack.
+   */
+  swap() {
+    if (this._top < 1) {
+      return;
+    }
+
+    const temp = this._storage[this._top];
+
+    this._storage[this._top] = this._storage[this._top - 1];
+    this._storage[this._top - 1] = temp;
+  }
+}
+
+module.exports = { Stack };
---
+++ src/stack/stack.test.js
@@ -0,0 +1,95 @@
+const { Stack } = require('./stack');
+
+describe('Stack', () => {
+  let stack;
+
+  test('should set empty storage and proper top pointer on construction', () => {
+    const stack = new Stack();
+    expect(stack._storage).toEqual([]);
+    expect(stack._top).toEqual(-1);
+  });
+
+  beforeEach(() => {
+    stack = new Stack();
+    stack._storage = [1, 'f', 5, 'test', 6, true, 19];
+    stack._top = 6;
+  });
+
+  describe('#peek', () => {
+    test('should return topmost element from the stack', () => {
+      expect(stack.peek()).toEqual(19);
+    });
+
+    test('should not remove the topmost element from the stack', () => {
+      stack.peek();
+      expect(stack.peek()).toEqual(19);
+    });
+  });
+
+  describe('#search', () => {
+    test('should return element\'s distance from the top of the stack', () => {
+      expect(stack.search(19)).toEqual(1);
+      expect(stack.search(true)).toEqual(2);
+      expect(stack.search('f')).toEqual(6);
+    });
+
+    test('should return -1 if the element could not have been found', () => {
+      expect(stack.search('sdfsdf')).toEqual(-1);
+      expect(stack.search(5654654)).toEqual(-1);
+      expect(stack.search(false)).toEqual(-1);
+    });
+  });
+
+  describe('#isEmpty', () => {
+    test('should return false if stack is not empty', () => {
+      expect(stack.isEmpty()).toBe(false);
+    });
+
+    test('should return true if stack is empty', () => {
+      stack._storage = [];
+      stack._top = -1;
+      expect(stack.isEmpty()).toBe(true);
+    });
+  });
+
+  describe('#size', () => {
+    test('should return stack\'s size', () => {
+      expect(stack.size()).toEqual(7);
+    });
+  });
+
+  describe('#push', () => {
+    test('should push element to the top of the stack', () => {
+      stack.push('push_test');
+      expect(stack.peek()).toEqual('push_test');
+    });
+
+    test('should return the added element', () => {
+      expect(stack.push('push_test')).toEqual('push_test');
+    });
+  });
+
+  describe('#pop', () => {
+    test('should remove the topmost element from the stack', () => {
+      stack.pop();
+      expect(stack.peek()).toEqual(true);
+    });
+
+    test('should return the removed element', () => {
+      expect(stack.pop()).toEqual(19);
+    });
+
+    test('should return null if stack was empty', () => {
+      stack._storage = [];
+      stack._top = -1;
+      expect(stack.pop()).toBeNull();
+    });
+  });
+
+  describe('#swap', () => {
+    test('should swap places of the top two element on the stack', () => {
+      stack.swap();
+      expect(stack._storage).toEqual([1, 'f', 5, 'test', 6, 19, true]);
+    });
+  });
+});
