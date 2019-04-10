2bc8c803f994a501699dd3f3250718f44cdecce4
Viktor <Nivelis91@gmail.com>
2019-04-10 22:44:34 +0200

    Improves and refactors Set: - Changes constructor argument from array to any iterable - Makes Set iterable - Now treats NaN as equal to other NaN and 0 as equal to -0 - Allows to specify custom separator for print() - Makes methods union, intersection, difference, isSuperset, isSubset static

--- src/set/set.js
+++ src/set/set.js
@@ -1,11 +1,48 @@
+const { normalizeZeros } = require('../helpers/normalize-zeros/normalize-zeros');
+const { isIterable } = require('../helpers/is-iterable/is-iterable');
+
 class Set {
   /**
    * Creates a new Set with initial values.
    *
-   * @param { ...* } initialValues
+   * @param { [iterable] } initialValues
    */
-  constructor(...initialValues) {
-    this._storage = initialValues || [];
+  constructor(initialValues = []) {
+    this._storage = [];
+
+    if (!isIterable(initialValues)) {
+      throw new TypeError('Argument initialValues is not an iterable object.');
+    }
+
+    for (let value of initialValues) {
+      this.add(value);
+    }
+  }
+
+  /**
+   * Returns a new Iterator object that contains an each element of the Set.
+   *
+   * @returns { { next: Function } }
+   */
+  [Symbol.iterator]() {
+    let index = -1;
+
+    return {
+      next: () => {
+        return {
+          value: this._storage[++index],
+          done: !(index in this._storage)
+        }
+      }
+    };
+  }
+
+  _find(item) {
+    item = normalizeZeros(item);
+
+    return this._storage.findIndex(currentItem => {
+      return Object.is(currentItem, item);
+    });
   }
 
   /**
@@ -17,7 +54,7 @@ class Set {
    */
   add(item) {
     if (!this.has(item)) {
-      this._storage.push(item);
+      this._storage.push(normalizeZeros(item));
       return true;
     }
 
@@ -32,7 +69,7 @@ class Set {
    * @returns { boolean } Returns false if item wasn't in the Set.
    */
   delete(item) {
-    const index = this._storage.indexOf(item);
+    const index = this._find(item);
 
     if (index !== -1) {
       this._storage.splice(index, 1);
@@ -40,67 +77,6 @@ class Set {
     }
 
     return false;
-  }
-
-  /**
-   * Returns an union of two Sets.
-   *
-   * @param { Set } set
-   *
-   * @returns { Set }
-   */
-  union(set) {
-    this._validateIfArgumentIsSet(set);
-
-    const newSet = new Set(...this.values());
-
-    set.values().forEach(item => {
-      newSet.add(item);
-    });
-
-    return newSet;
-  }
-
-  /**
-   * Returns an intersection of two Sets.
-   *
-   * @param { Set } set
-   *
-   * @returns { Set }
-   */
-  intersection(set) {
-    this._validateIfArgumentIsSet(set);
-
-    const newSet = new Set();
-
-    this._storage.forEach(item => {
-      if (set.has(item)) {
-        newSet.add(item);
-      }
-    });
-
-    return newSet;
-  }
-
-  /**
-   * Returns an new Set containing all the items of B that are not in A, where B is the given set.
-   *
-   * @param { Set } set
-   *
-   * @returns { Set }
-   */
-  difference(set) {
-    this._validateIfArgumentIsSet(set);
-
-    const newSet = new Set();
-
-    set.values().forEach(item => {
-      if (!this.has(item)) {
-        newSet.add(item);
-      }
-    });
-
-    return newSet;
   }
 
   /**
@@ -118,7 +94,7 @@ class Set {
    * @returns { boolean }
    */
   has(item) {
-    return this._storage.indexOf(item) !== -1;
+    return this._find(item) !== -1;
   }
 
   /**
@@ -128,36 +104,6 @@ class Set {
    */
   values() {
     return this._storage;
-  }
-
-  /**
-   * Checks whether the Set is a superset of the given set.
-   *
-   * @param { Set } set
-   *
-   * @returns { boolean }
-   */
-  isSupersetOf(set) {
-    this._validateIfArgumentIsSet(set);
-
-    return set.values().every(item => {
-      return this.has(item);
-    });
-  }
-
-  /**
-   * Checks whether the Set is a subset of the given set.
-   *
-   * @param { Set } set
-   *
-   * @returns { boolean }
-   */
-  isSubsetOf(set) {
-    this._validateIfArgumentIsSet(set);
-
-    return this._storage.every(item => {
-      return set.has(item);
-    });
   }
 
   /**
@@ -179,24 +125,140 @@ class Set {
   }
 
   /**
-   * Returns all Set's items separated by space.
+   * Returns all Set's items.
+   *
+   * @param { string } separator
    *
    * @returns { string }
    */
-  print() {
-    return this._storage.join(' ');
+  print(separator = ' ') {
+    return this._storage.join(separator);
+  }
+
+  /**
+   * Returns an union of set1 and set2.
+   *
+   * @static
+   *
+   * @param { Set } set1
+   * @param { Set } set2
+   *
+   * @returns { Set }
+   */
+  static union(set1, set2) {
+    Set._validateIfArgumentIsSet(set1);
+    Set._validateIfArgumentIsSet(set2);
+
+    const unionSet = new Set(set1.values());
+
+    set2.values().forEach(item => {
+      unionSet.add(item);
+    });
+
+    return unionSet;
+  }
+
+  /**
+   * Returns an intersection of set1 and set2.
+   *
+   * @static
+   *
+   * @param { Set } set1
+   * @param { Set } set2
+   *
+   * @returns { Set }
+   */
+  static intersection(set1, set2) {
+    Set._validateIfArgumentIsSet(set1);
+    Set._validateIfArgumentIsSet(set2);
+
+    const intersectionSet = new Set();
+
+    set1.values().forEach(item => {
+      if (set2.has(item)) {
+        intersectionSet.add(item);
+      }
+    });
+
+    return intersectionSet;
+  }
+
+  /**
+   * Returns an new Set containing all the items of set1 that are not in set2.
+   *
+   * @static
+   *
+   * @param { Set } set1
+   * @param { Set } set2
+   *
+   * @returns { Set }
+   */
+  static difference(set1, set2) {
+    Set._validateIfArgumentIsSet(set1);
+    Set._validateIfArgumentIsSet(set2);
+
+    const diffSet = new Set();
+
+    set2.values().forEach(item => {
+      if (!set1.has(item)) {
+        diffSet.add(item);
+      }
+    });
+
+    return diffSet;
+  }
+
+  /**
+   * Checks whether the set1 is a superset of set2.
+   *
+   * @static
+   *
+   * @param { Set } set1
+   * @param { Set } set2
+   *
+   * @returns { boolean }
+   */
+  static isSuperset(set1, set2) {
+    Set._validateIfArgumentIsSet(set1);
+    Set._validateIfArgumentIsSet(set2);
+
+    return set2.values().every(item => {
+      return set1.has(item);
+    });
+  }
+
+  /**
+   * Checks whether set1 is subset of set2.
+   *
+   * @static
+   *
+   * @param { Set } set1
+   * @param { Set } set2
+   *
+   * @returns { boolean }
+   */
+  static isSubset(set1, set2) {
+    Set._validateIfArgumentIsSet(set1);
+    Set._validateIfArgumentIsSet(set2);
+
+    return set1.values().every(item => {
+      return set2.has(item);
+    });
   }
 
   /**
    * Throws and error if passed argument is not an instance of Set.
    *
+   * @static
+   * @private
+   *
    * @param { * } arg
    *
-   * @throws Error
+   * @throws TypeError
    */
-  _validateIfArgumentIsSet(arg) {
+  static _validateIfArgumentIsSet(arg) {
     if (!(arg instanceof Set)) {
-      throw new Error('Argument \'set\' must be an instance of Set.');
+      throw new TypeError('Argument must be an instance of Set.');
     }
   }
 }
--- src/set/set.test.js
+++ src/set/set.test.js
@@ -3,19 +3,48 @@ const { Set } = require('./set');
 describe('Set', () => {
   let set;
 
-  test('should create empty storage on construction if no argument were passed', () => {
-    const set = new Set();
-    expect(set._storage).toEqual([]);
-  });
-
-  test('should create initially filled set on construction if arguments were passed', () => {
-    const set = new Set(1, 2, 3);
-    expect(set._storage).toEqual([1, 2, 3]);
-  });
-
   beforeEach(() => {
     set = new Set();
     set._storage = [1, 'f', 5, 'test', 6, true, 19];
+  });
+
+  test('should be iterable', () => {
+    let items = [];
+
+    for (let item of set) {
+      items.push(item);
+    }
+
+    expect(items).toEqual([1, 'f', 5, 'test', 6, true, 19]);
+  });
+
+  describe('#constructor', () => {
+    test('should create empty storage on construction if no argument were passed', () => {
+      const set = new Set();
+      expect(set._storage).toEqual([]);
+    });
+
+    test('should create initially filled set on construction if array was passed', () => {
+      const set = new Set([1, 2, 3]);
+      expect(set._storage).toEqual([1, 2, 3]);
+    });
+
+    test('should create initially filled set on construction if iterable object was passed', () => {
+      const iterable = {
+        *[Symbol.iterator]() {
+          yield 2;
+          yield 4;
+          yield 6;
+        }
+      };
+
+      const set = new Set(iterable);
+      expect(set._storage).toEqual([2, 4, 6]);
+    });
+
+    test('should throw if non-iterable object was passed', () => {
+      expect(() => {new Set(1, 2, 3)}).toThrow();
+    });
   });
 
   describe('#add', () => {
@@ -25,10 +54,14 @@ describe('Set', () => {
     });
 
     test('should not add element to the set if it already exists and return false', () => {
+      set.add(-0);
+      set.add(NaN);
       const initialSetLength = set.size();
       expect(set.add(6)).toBe(false);
       expect(set.add(6)).toBe(false);
       expect(set.add(6)).toBe(false);
+      expect(set.add(-0)).toBe(false);
+      expect(set.add(NaN)).toBe(false);
       expect(set.size()).toEqual(initialSetLength);
     });
   });
@@ -47,25 +80,25 @@ describe('Set', () => {
 
   describe('#union', () => {
     test('should return new set that is the union of 2 sets', () => {
-      const anotherSet = new Set(5, 'test2', 123);
-      const union = set.union(anotherSet);
+      const set2 = new Set([5, 'test2', 123]);
+      const union = Set.union(set, set2);
       expect(union._storage).toEqual([1, 'f', 5, 'test', 6, true, 19, 'test2', 123]);
     });
   });
 
   describe('#intersection', () => {
     test('should return a new set: A ∩ B', () => {
-      const a = new Set(0, 2, 4, 6, 8, 10);
-      const b = new Set(1, 2, 3, 5, 8, 13);
-      expect(a.intersection(b).values()).toEqual([2, 8]);
+      const set1 = new Set([0, 2, 4, 6, 8, 10]);
+      const set2 = new Set([1, 2, 3, 5, 8, 13]);
+      expect(Set.intersection(set1, set2).values()).toEqual([2, 8]);
     });
   });
 
   describe('#difference', () => {
     test('should return a new set that is equal to B \ A', () => {
-      const a = new Set(0, 2, 4, 6, 8, 10);
-      const b = new Set(1, 2, 3, 5, 8, 13);
-      expect(a.difference(b).values()).toEqual([1, 3, 5, 13]);
+      const set1 = new Set([0, 2, 4, 6, 8, 10]);
+      const set2 = new Set([1, 2, 3, 5, 8, 13]);
+      expect(Set.difference(set1, set2).values()).toEqual([1, 3, 5, 13]);
     });
   });
 
@@ -78,15 +111,21 @@ describe('Set', () => {
 
   describe('#has', () => {
     test('should return true if set has the item', () => {
+      set.add(NaN);
+      set.add(-0);
       expect(set.has(5)).toBe(true);
       expect(set.has('test')).toBe(true);
       expect(set.has(true)).toBe(true);
+      expect(set.has(NaN)).toBe(true);
+      expect(set.has(-0)).toBe(true);
     });
 
     test('should return false if set does not have the item', () => {
       expect(set.has(6456456)).toBe(false);
       expect(set.has('dsfgdfsgdf')).toBe(false);
       expect(set.has(false)).toBe(false);
+      expect(set.has(NaN)).toBe(false);
+      expect(set.has(-0)).toBe(false);
     });
   });
 
@@ -96,27 +135,27 @@ describe('Set', () => {
     });
   });
 
-  describe('#isSupersetOf', () => {
-    test('should return true if set is superset of given set', () => {
-      const subset = new Set(19, 5, 6, 'f');
-      expect(set.isSupersetOf(subset)).toBe(true);
+  describe('#isSuperset', () => {
+    test('should return true if set1 is superset set2', () => {
+      const subset = new Set([19, 5, 6, 'f']);
+      expect(Set.isSuperset(set, subset)).toBe(true);
     });
 
-    test('should return false if set is not superset of given set', () => {
-      const supposedSubset = new Set('dfgdfg', 1234957);
-      expect(set.isSupersetOf(supposedSubset)).toBe(false);
+    test('should return false if set1 is not superset set2', () => {
+      const supposedSubset = new Set(['dfgdfg', 1234957]);
+      expect(Set.isSuperset(set, supposedSubset)).toBe(false);
     });
   });
 
-  describe('#isSubsetOf', () => {
-    test('should return true if set is subset of given set', () => {
-      const superset = new Set(19, 'sdafh', 5, 6, 'f', true, 1, 'test', 21, 'xxx', false, 34654);
-      expect(set.isSubsetOf(superset)).toBe(true);
+  describe('#isSubset', () => {
+    test('should return true if set1 is subset of set2', () => {
+      const superset = new Set([19, 'sdafh', 5, 6, 'f', true, 1, 'test', 21, 'xxx', false, 34654]);
+      expect(Set.isSubset(set, superset)).toBe(true);
     });
 
-    test('should return false if set is not subset of given set', () => {
-      const supposedSuperset = new Set(5, 6, 7, 8, 9, 10, '345');
-      expect(set.isSubsetOf(supposedSuperset)).toBe(false);
+    test('should return false if set1 is not subset of set2', () => {
+      const supposedSuperset = new Set([5, 6, 7, 8, 9, 10, '345']);
+      expect(Set.isSubset(set, supposedSuperset)).toBe(false);
     });
   });
 
@@ -140,8 +179,12 @@ describe('Set', () => {
   });
 
   describe('#print', () => {
-    test('should print the set', () => {
+    test('should print the set with default space separator', () => {
       expect(set.print()).toEqual('1 f 5 test 6 true 19');
+    });
+
+    test('should print the set with custom separator', () => {
+      expect(set.print('-')).toEqual('1-f-5-test-6-true-19');
     });
   });
 });
